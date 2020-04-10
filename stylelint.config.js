73e54b75414eabf17a10d979eed517953d50753f
Carlos Valverde <studio@carlos-valverde.com>
2020-04-10 20:02:42 +0200

    [0.3.1][ADDED] Stylelint rules

--- stylelint.config.js
+++ stylelint.config.js
@@ -4,6 +4,22 @@ module.exports = {
   rules: {
     "selector-nested-pattern": "^&",
     indentation: 2,
+    "declaration-block-single-line-max-declarations": 1,
+    "block-opening-brace-space-before": "always",
+    "block-closing-brace-newline-after": "always",
+    "declaration-colon-space-after": "always",
+    "declaration-block-trailing-semicolon": "always",
+    "value-list-comma-space-after": "always-single-line",
+    "number-leading-zero": "never",
+    "color-hex-case": "lower",
+    "color-hex-length": "short",
+    "selector-no-qualifying-type": [
+        true,
+      {
+        "ignore": ["attribute"]
+      },
+    ],
+    "length-zero-no-unit": true,
     "no-descending-specificity": null,
     "no-eol-whitespace": null,
     "declaration-empty-line-before": null,
